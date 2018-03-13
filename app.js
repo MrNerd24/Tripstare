81c99239bfdf132400b3e5c0d7faf83d0728f0a7
MrNerd24 <juuso.lassila@helsinki.fi>
2018-03-13 10:35:18 +0200

    Configuring

--- app.js
+++ app.js
@@ -19,8 +19,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/api', index);
+
+app.use(express.static('frontend/build'))
+// app.use('/', index);
 app.use('/api/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
